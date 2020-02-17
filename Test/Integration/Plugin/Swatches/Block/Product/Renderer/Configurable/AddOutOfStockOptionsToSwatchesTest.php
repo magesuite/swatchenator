c5bb9c62bcb273471d7403fd08aad5c343537d2d
Błażej Dołęska <blazej.doleska@creativestyle.pl>
2020-02-17 08:18:41 +0000

    Added plugin to add out of stock options

---
+++ Test/Integration/Plugin/Swatches/Block/Product/Renderer/Configurable/AddOutOfStockOptionsToSwatchesTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace MageSuite\Swatchenator\Test\Integration\Plugin\Swatches\Block\Product\Renderer\Configurable;
+
+class AddOutOfStockOptionsToSwatchesTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var \Magento\TestFramework\ObjectManager
+     */
+    protected $objectManager;
+
+    /**
+     * @var \Magento\Swatches\Block\Product\Renderer\Configurable
+     */
+    protected $swatchRenderer;
+
+    /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    protected $productRepository;
+
+    public function setUp()
+    {
+        $this->objectManager = \Magento\TestFramework\ObjectManager::getInstance();
+
+        $this->swatchRenderer = $this->objectManager->create(\Magento\Swatches\Block\Product\Renderer\Configurable::class);
+        $this->productRepository = $this->objectManager->create(\Magento\Catalog\Api\ProductRepositoryInterface::class);
+    }
+
+    /**
+     * @magentoAppArea frontend
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     * @magentoConfigFixture current_store swatchenator/general/is_enabled 1
+     * @magentoDataFixture Magento/ConfigurableProduct/_files/product_configurable.php
+     * @magentoDataFixture modifySimpleProducts
+     */
+    public function testItAddOutOfStockOptionsToSwatchesConfig()
+    {
+        $product = $this->productRepository->get('configurable');
+        $swatchRenderer = $this->swatchRenderer->setProduct($product);
+
+        $jsonConfig = $swatchRenderer->getJsonConfig();
+        $jsonConfig = json_decode($jsonConfig, true);
+
+        $attributeData = array_shift($jsonConfig['attributes']);
+
+        $firstOption = array_shift($attributeData['options']);
+
+        $this->assertEmpty($firstOption['products']);
+    }
+
+    public static function modifySimpleProducts()
+    {
+        require __DIR__.'/../../../../../../../_files/products.php';
+
+        $indexerRegistry = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+            ->create(\Magento\Framework\Indexer\IndexerRegistry::class);
+        $indexerRegistry->get(\Magento\CatalogSearch\Model\Indexer\Fulltext::INDEXER_ID)->reindexAll();
+    }
+}
