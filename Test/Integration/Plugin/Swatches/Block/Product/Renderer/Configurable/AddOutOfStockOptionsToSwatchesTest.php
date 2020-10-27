c5f815b6a4b075947b80e58a1390daa82bfba4db
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2020-10-27 07:20:50 +0100

    Compatibility with PHPUNIT 9

--- Test/Integration/Plugin/Swatches/Block/Product/Renderer/Configurable/AddOutOfStockOptionsToSwatchesTest.php
+++ Test/Integration/Plugin/Swatches/Block/Product/Renderer/Configurable/AddOutOfStockOptionsToSwatchesTest.php
@@ -19,7 +19,7 @@ class AddOutOfStockOptionsToSwatchesTest extends \PHPUnit\Framework\TestCase
      */
     protected $productRepository;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->objectManager = \Magento\TestFramework\ObjectManager::getInstance();
 
