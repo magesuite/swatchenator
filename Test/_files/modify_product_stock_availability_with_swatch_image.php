2a71eb84c43678ffd4b8ae7e0d7716af88eef33e
Dariusz Twardowski <dariusz.twardowski@creativestyle.pl>
2021-06-10 10:40:49 +0000

    Hotfix for tests loading

--- Test/_files/modify_product_stock_availability_with_swatch_image.php
+++ Test/_files/modify_product_stock_availability_with_swatch_image.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-\Magento\TestFramework\Workaround\Override\Fixture\Resolver::getInstance()->requireDataFixture('Magento/Catalog/_files/product_image.php');
+include BP.'/dev/tests/integration/testsuite/Magento/Catalog/_files/product_image.php';
 
 $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
 
